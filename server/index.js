aa1441986f869284478de4587756436b27a7e5f0
Mayank <mayankagarwal2789@gmail.comk>
2024-09-29 18:42:52 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express =require('express')
+const bcrypt = require('bcryptjs');
 const cors =require('cors')
 const userRoutes = require("./routes/userRoutes");
 const messageRoutes = require("./routes/messages");
